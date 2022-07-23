7b58b3768e76440f4b962873b575f62bf3f651db
zhanbolat_nurutdin <zh.nurutdin@darwintrade.kz>
2022-07-23 12:25:51 +0600

    update method in library;

--- libs/http/http.go
+++ libs/http/http.go
@@ -5,17 +5,25 @@ const Data = "data"
 
 type Resp map[string]interface{}
 
-func Unauthorized(payload ...interface{}) (Resp, error) {
-	if len(payload) == 0 {
-		return Resp{
-			Error: "unauthorized",
-		}, nil
-	}
-	return Resp{
-		Error: payload,
-	}, nil
+func Unauthorized(payload ...interface{}) Resp {
+	return error("unauthorized", payload...)
+}
+
+func InternalServErr(payload ...interface{}) Resp {
+	return error("internal server error", payload...)
 }
 
 func Success(payload interface{}) Resp {
 	return Resp{"data": payload}
+}
+
+func error(def string, payload ...interface{}) Resp {
+	if len(payload) == 0 {
+		return Resp{
+			Error: def,
+		}
+	}
+	return Resp{
+		Error: payload,
+	}
 }
