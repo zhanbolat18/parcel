6bca48ce5ef7ceb5f4bf1e7cf13fee6ed7f1e764
zhanbolat_nurutdin <zh.nurutdin@darwintrade.kz>
2022-07-23 12:12:20 +0600

    update method in library;

--- libs/http/http.go
+++ libs/http/http.go
@@ -1,11 +1,19 @@
 package http
 
+const Error = "error"
+const Data = "data"
+
 type Resp map[string]interface{}
 
-func Unauthorized() Resp {
-	return Resp{
-		"error": "unauthorized",
+func Unauthorized(payload ...interface{}) (Resp, error) {
+	if len(payload) == 0 {
+		return Resp{
+			Error: "unauthorized",
+		}, nil
 	}
+	return Resp{
+		Error: payload,
+	}, nil
 }
 
 func Success(payload interface{}) Resp {
